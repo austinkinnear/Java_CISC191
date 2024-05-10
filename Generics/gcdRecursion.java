eaefd2bfe395dbb0c392939a5e4e9a80267c80a7
austinkinnear <austinkinnear1@gmail.com>
2024-05-10 12:38:49 -0700

    finished recursion GCD method

--- Generics/gcdRecursion.java
+++ Generics/gcdRecursion.java
@@ -1,27 +1,30 @@
+/** 
+ * Author: Austin Kinnear, CISC 191 Intermediate Java Programming  
+ * Programming Project: Recursion method to find GCD
+*/
 import java.util.Scanner;
-
 public class gcdRecursion {
 
-    // Recursive method to calculate the GCD of two numbers
+    // Using Recursion to calculate the GCD
     public static int gcd(int m, int n) {
         if (n == 0) {
-            return m;  // Base case: if m % n is 0, return n
+            return m; 
         } else {
-            return gcd(n, m % n);  // Recursive call
+            return gcd(n, m % n);  
         }
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Prompting user to enter two integers
+        // User prompt so we can test for 3 and 7 
         System.out.print("Enter the first number: ");
-        int num1 = scanner.nextInt();
+        int num1 = scanner.nextInt(); 
         System.out.print("Enter the second number: ");
         int num2 = scanner.nextInt();
         scanner.close();
 
-        // Displaying the GCD
+        // Basic print statement which will be seen in the terminal
         System.out.println("The GCD of " + num1 + " and " + num2 + " is " + gcd(num1, num2));
     }
 }
