ffdd29c0720a1b70c5b0c711393a72be542f2d2a
austinkinnear <austinkinnear1@gmail.com>
2024-05-13 13:01:11 -0700

    beggining of merge sort alogtithms in Java

---
+++ MergeSorting/MergeSort.java
@@ -0,0 +1,102 @@
+/**
+ * Author: Austin Kinnear, CISC 191 Intermediate Java Programming
+ * Programming Project: Creating a merge sorting algorithm
+ */
+import java.util.Scanner;
+
+public class MergeSort {
+    // Main function that sorts arr[l..r] using merge()
+    void sort(int arr[], int l, int r) {
+        if (l < r) {
+            // Find the middle point
+            int m = (l + r) / 2;
+            
+            // Sort first and second halves
+            sort(arr, l, m);
+            sort(arr, m + 1, r);
+            
+            // Merge the sorted halves
+            merge(arr, l, m, r);
+        }
+    }
+    
+    // Function to merge the two halves arr[l..m] and arr[m+1..r]
+    void merge(int arr[], int l, int m, int r) {
+        int n1 = m - l + 1;
+        int n2 = r - m;
+
+        // Create temp arrays
+        int L[] = new int[n1];
+        int R[] = new int[n2];
+
+        // Copy data to temp arrays
+        for (int i = 0; i < n1; ++i)
+            L[i] = arr[l + i];
+        for (int j = 0; j < n2; ++j)
+            R[j] = arr[m + 1 + j];
+
+        // Merge the temp arrays
+
+        // Initial indexes of first and second subarrays
+        int i = 0, j = 0;
+
+        // Initial index of merged subarray array
+        int k = l;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        // Copy remaining elements of L[] if any
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        // Copy remaining elements of R[] if any
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
+    // Utility function to print the array
+    static void printArray(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+
+    // Driver method to take user input and sort
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter number of elements:");
+        int n = scanner.nextInt();
+        
+        int arr[] = new int[n];
+        System.out.println("Enter the elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        
+        System.out.println("Given Array:");
+        printArray(arr);
+
+        MergeSort ob = new MergeSort();
+        ob.sort(arr, 0, arr.length - 1);
+
+        System.out.println("\nSorted array:");
+        printArray(arr);
+        
+        scanner.close();
+    }
+}
